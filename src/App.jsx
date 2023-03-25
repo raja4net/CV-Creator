bd9364cc67911242a96729a4cdc0a6718ae7e3b5
Raja Rupinder Singh <rajarupinder@gmail.com>
2023-03-25 15:33:33 +0530

    CV Creator Header file created

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './styles/App.css'
+import Header from './components/Header.jsx'
+
+function App() {
+  return (
+    <div>
+    <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return(
+        <>
+            <div className="header">
+            <h1>CV Creator</h1>
+            </div>
+        </>
+    )
+}
+
+export default Header
